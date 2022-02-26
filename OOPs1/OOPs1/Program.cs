9a94305dfaa53c41d462062a4d6f40534a4bb9db
Saikat0150 <100399205+Saikat0150@users.noreply.github.com>
2022-02-26 16:29:11 +0530

    Add files via upload

---
+++ OOPs1/OOPs1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs1
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public void PersonDetails()
+        {
+            Console.WriteLine("You are " + name + " and " + age + " years old");
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Console.WriteLine("Enter your name: ");
+            p.name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            p.age = Convert.ToInt32(Console.ReadLine());
+            p.PersonDetails();
+            Console.Read();
+        }
+    }
+}
